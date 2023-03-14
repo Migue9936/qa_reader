171a75956ee3c95ade807763a923b14a1f226c27
migue9936 <miguelcortes9936@gmail.com>
2023-03-14 16:02:51 -0500

    begining to develop and initialize the DB

---
+++ lib/models/scan_model.dart
@@ -0,0 +1,40 @@
+// To parse this JSON data, do
+//
+//     final scanModel = scanModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+
+String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+
+class ScanModel {
+    ScanModel({
+        this.id,
+        required this.type,
+        required this.value,
+    }){
+      if (type.contains('http')) {
+
+        type ='http';
+      }else{
+        type = 'geo';
+      }
+    }
+
+    int? id;
+    String type;
+    String value;
+
+    factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        type: json["type"],
+        value: json["value"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "id": id,
+        "type": type,
+        "value": value,
+    };
+}
--- lib/widgets/floating_navigation_button.dart
+++ lib/widgets/floating_navigation_button.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
 class FloatingNavigationButtom extends StatelessWidget {
+
+  
   const FloatingNavigationButtom({super.key});
 
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+          String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode('#FFC107','Cancel ', false,ScanMode.QR);
+          print(barcodeScanRes);
+      },
       child: const Icon(Icons.qr_code_scanner_rounded),
       );
   }
